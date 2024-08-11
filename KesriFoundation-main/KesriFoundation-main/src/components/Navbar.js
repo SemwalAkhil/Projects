d00f2ec78ae576099513473f5a1b7d5b440c97f4
SemwalAkhil <148767471+SemwalAkhil@users.noreply.github.com>
2024-08-11 20:02:47 +0530

    Update Navbar.js
    Changed tagline

--- KesriFoundation-main/KesriFoundation-main/src/components/Navbar.js
+++ KesriFoundation-main/KesriFoundation-main/src/components/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
             <Image className='h-40 w-40 rounded-full bg-transparent max-lg:h-20 max-lg:w-20 object-cover lg:mt-9 md:mt-4' src='/images/logo.png' width={150} height={150}/>
             <div className='flex flex-col pr-40 max-lg:pr-0'>
                 <h1 className='h-auto'><span className='text-blue-900 text-6xl max-lg:text-3xl' >Kesri Foundation</span></h1>
-                <p className='text-blue-500 font-bold text-2xl ml-1 max-lg:text-sm max-lg:ml-0 max-lg:mr-1 max-lg:text-start'>Knowing Yourself and Empowering Lives.....</p>
+                <p className='text-blue-500 font-bold text-2xl ml-1 max-lg:text-sm max-lg:ml-0 max-lg:mr-1 max-lg:text-start'>Empowering Lives</p>
             </div>
         </div>
 
