8108e1adf11c50647b82d9c848cbc29376770332
SemwalAkhil <akhil392semwal@gmail.com>
2024-09-05 14:24:26 +0530

    Updating KesriSite 5/9/24,14:24

--- KesriFoundation-main/KesriFoundation-main/src/components/Navig.js
+++ KesriFoundation-main/KesriFoundation-main/src/components/Navig.js
@@ -5,20 +5,20 @@ export const Navig = () => {
   const[open,setOpen]=useState(false); 
   return (
     <>
-    <div className='hidden max-lg:flex max-lg:flex-col max-lg:w-full max-lg:justify-center max-lg:items-center max-lg:mt-5'>
+    <div className='hidden max-lg:flex max-lg:flex-col max-lg:w-full max-lg:justify-end max-lg:items-end max-lg:mt-5 max-lg:pr-5'>
       <div className="container" onClick={() => setOpen(!open)}>
       <div className={`line ${open ? 'line-1-open' : 'line-1-close'}`}></div>
       <div className={`line ${open ? 'line-2-open' : 'line-2-close'}`}></div>
       <div className={`line ${open ? 'line-3-open' : 'line-3-close'}`}></div>
     </div>
-    {open&&(<div className='flex flex-row justify-center bg-white max-lg:flex max-lg:flex-col max-lg:justify-center max-lg:items-center'>
+    {open&&(<div className='flex flex-col bg-white max-lg:items-end'>
 
-        <Link href='/'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 hover:text-white rounded-full p-5'><button>Home</button></div></Link>
-        <Link href='/Aboutus'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full p-5 hover:text-white'><button>About Kesri Foundation</button></div></Link>
-        <Link href='/Service'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full p-5 hover:text-white'><button>What We Provide</button></div></Link>
-        <Link href='/Accredition'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full p-5 hover:text-white'><button>Our Accredition</button></div></Link>
-        <Link href='/Gallerys'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full p-5 hover:text-white'><button>Glimpse of Our Events</button></div></Link>
-        <Link href='/ContactUs'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full p-5 hover:text-white'><button>Reach Out To Us</button></div></Link>
+        <Link href='/'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 hover:text-white rounded-full '><button>Home</button></div></Link>
+        <Link href='/Aboutus'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full  hover:text-white'><button>About Kesri Foundation</button></div></Link>
+        <Link href='/Service'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full  hover:text-white'><button>What We Provide</button></div></Link>
+        <Link href='/Accredition'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full  hover:text-white'><button>Our Accredition</button></div></Link>
+        <Link href='/Gallerys'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full  hover:text-white'><button>Glimpse of Our Events</button></div></Link>
+        <Link href='/ContactUs'><div className='px-10 text-xl bg-transparent font-serif font-bold text-blue-900 hover:bg-blue-900 rounded-full  hover:text-white'><button>Reach Out To Us</button></div></Link>
     </div>)}
     </div>
     <div className='flex flex-row justify-center bg-white max-lg:hidden'>
