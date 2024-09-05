c08c62285b5e0ff92c0af4bb6f8f38a39bb05513
SemwalAkhil <akhil392semwal@gmail.com>
2024-09-05 09:57:08 +0530

    Updating KesriFoundation-main 5/9/24,9:56

--- KesriFoundation-main/KesriFoundation-main/src/components/Contact.js
+++ KesriFoundation-main/KesriFoundation-main/src/components/Contact.js
@@ -57,7 +57,7 @@ export const Contact = () => {
       <form className='mt-8' onSubmit={(e)=>{handleSubmit(e)}}>
     <div className="grid gap-6 mb-6 md:grid-cols-2">
         <div>
-            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
             <input type="text" pattern="[A-Za-z]+" id="first_name" onChange={(e)=>{setFirstName(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter First Name" required/>
         </div>
        
