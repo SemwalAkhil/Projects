a27e7dacc7523c47c9aa93672e4be12698516d5a
SemwalAkhil <akhil392semwal@gmail.com>
2024-09-24 20:30:47 +0530

    adding queue 24/9/24;20:30

--- C SITE/CODE/QUEUE/1.c
+++ C SITE/CODE/QUEUE/1.c
@@ -1,16 +1,65 @@
 // 1. IMPLEMENT THE OPERATIONS OF SIMPLE QUEUE IN AN INTEGER ARRAY (INSERT, DELETE, PEEK, ISEMPTY, ISFULL)
 #include <stdio.h>
-#define size 100
-void insert() {}
-void delete() {}
-void peek() {}
-void isEmpty() {}
-void isFull(){}
+#define size 5
+void insert(int *queue, int *front, int *rear, int value)
+{
+    if ((*front <= *rear) && (*rear != size))
+    {
+        queue[(*rear)++] = value;
+    }
+    else
+    {
+        printf("Queue Full\n");
+    }
+}
+void delete(int *queue, int *front, int *rear)
+{
+    if ((*front < *rear) && (*front != size))
+    {
+        printf("%d\n", queue[(*front)++]);
+    }
+    else
+    {
+        printf("Queue Empty\n");
+    }
+}
+void peek(int *queue, int *front, int *rear)
+{
+    if ((*front < *rear) && (*front != size))
+    {
+        printf("%d\n", queue[*front]);
+    }
+    else
+    {
+        printf("Queue Empty\n");
+    }
+}
+void isEmpty(int *queue, int *front, int *rear)
+{
+    if ((*front < *rear) && (*front != size))
+    {
+        printf("Queue Not Empty\n");
+    }
+    else
+    {
+        printf("Queue Empty\n");
+    }
+}
+void isFull(int *queue, int *front, int *rear)
+{
+    if ((*front <= *rear) && (*rear != size))
+    {
+        printf("Queue Not Full\n");
+    }
+    else
+    {
+        printf("Queue Full\n");
+    }
+}
 int main()
 {
     int queue[size];
-    int value,int front;
-    int choice = 4;
+    int value, front = 0, rear = 0, choice = 4;
     while (1)
     {
         printf("\n1.INSERT\n2.DELETE\n3.PEEK\n4.ISEMPTY\n5.ISFULL\n6.EXIT\nENTER THE OPERATION: ");
@@ -20,19 +69,19 @@ int main()
         case 1:
             printf("ENTER THE VALUE: ");
             scanf("%d", &value);
-            insert(queue, front, rear, value);
+            insert(queue, &front, &rear, value);
             break;
         case 2:
-            delete (queue);
+            delete (queue, &front, &rear);
             break;
         case 3:
-            peek(queue);
+            peek(queue, &front, &rear);
             break;
         case 4:
-            isEmpty(queue);
+            isEmpty(queue, &front, &rear);
             break;
         case 5:
-            isFull(queue);
+            isFull(queue, &front, &rear);
             break;
         case 6:
             return 0;
