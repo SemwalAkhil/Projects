41efacf12a6c2c7471e53794acdc664e6e08d7d1
SemwalAkhil <akhil392semwal@gmail.com>
2024-09-26 18:08:43 +0530

    updated dyanmic queue 26/9/24;18:08

--- C SITE/CODE/QUEUE/3.c
+++ C SITE/CODE/QUEUE/3.c
@@ -23,29 +23,34 @@ int isEmpty(struct node *start)
         return 1;
     return 0;
 }
-void insert(struct node *start, struct node *end, int val)
+int insert(struct node *start, struct node *end, int val)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     if (temp != NULL)
     {
-        temp->next = end->next;
+        temp->next = NULL;
         temp->data = val;
+        if (end->next != NULL)
+        {
+            end->next->next = temp;
+        }
         end->next = temp;
         if (start->next == NULL)
         {
             start->next = temp;
         }
-        // printf("%d\n", end->next->data);
+        return 1;
     }
+    return 0;
 }
 void delete(struct node *start)
 {
-    if (!(isEmpty(start)))
+    if (start->next != NULL)
     {
         struct node *temp = (struct node *)malloc(sizeof(struct node));
         printf("%d", start->next->data);
         temp = start->next;
-        start->next = temp->next;
+        start->next = start->next->next;
         free(temp);
     }
     else
@@ -55,7 +60,7 @@ void delete(struct node *start)
 }
 void peek(struct node *start)
 {
-    if (!(isEmpty(start)))
+    if (start->next != NULL)
     {
         printf("%d", start->next->data);
     }
@@ -80,7 +85,8 @@ int main()
         case 1:
             printf("ENTER THE VALUE: ");
             scanf("%d", &val);
-            insert(start, end, val);
+            if (!insert(start, end, val))
+                printf("QUEUE IS FULL");
             break;
         case 2:
             delete (start);
@@ -102,91 +108,3 @@ int main()
     }
     return 0;
 }
-/*
-    f.next -> r -> null
-    r -> temp , temp.next-> null
-*/
-// #include <stdio.h>
-// #include <stdlib.h>
-// struct que
-// {
-//     int val;
-//     struct que *next;
-// };
-// struct que *front = NULL, *rear, *temp;
-// void push(int val)
-// {
-//     if (front == NULL)
-//     {
-//         front = (struct que *)malloc(2 * sizeof(int));
-//         front->val = val;
-//         front->next = NULL;
-//         rear = front;
-//     }
-//     else
-//     {
-//         temp = (struct que *)malloc(2 * sizeof(int));
-//         if (temp != NULL)
-//         {
-//             temp->val = val;
-//             temp->next = NULL;
-//             rear->next = temp;
-//             rear = temp;
-//         }
-//         else
-//         {
-//             printf("queue full");
-//         }
-//     }
-// }
-// void pop()
-// {
-//     struct que *tempo;
-//     if (front == NULL)
-//     {
-//         printf("empty");
-//     }
-//     else
-//     {
-//         printf("%d", front->val);
-//         if (front == rear)
-//         {
-//             front = NULL;
-//             rear = NULL;
-//             free(front);
-//         }
-//         else
-//         {
-//             tempo = front->next;
-//             free(front);
-//             front = tempo;
-//         }
-//     }
-// }
-// int main()
-// {
-//     while (1)
-//     {
-//         int c;
-//         printf("choice  : ");
-//         scanf("%d", &c);
-//         if (c == 1)
-//         {
-//             scanf("%d", &c);
-//             push(c);
-//         }
-//         else if (c == 2)
-//         {
-//             pop();
-//         }
-//         else if (c == 3)
-//         {
-//             break;
-//         }
-//     }
-// }
-
-/*
-    s.next 20
-    temp 30
-*/
