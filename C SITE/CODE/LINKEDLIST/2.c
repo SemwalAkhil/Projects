3a971d9cf8f51499d38b0a0b78a3f24a81909c99
SemwalAkhil <akhil392semwal@gmail.com>
2024-10-21 20:58:30 +0530

    updating c site;21/10/24;20:58

--- C SITE/CODE/LINKEDLIST/2.c
+++ C SITE/CODE/LINKEDLIST/2.c
@@ -6,7 +6,7 @@ struct node
     struct node *next;
     int data;
 };
-int insertb(struct node *start, int val)
+int insertb(struct node *start, struct node *end, int val)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     if (temp != NULL)
@@ -14,10 +14,12 @@ int insertb(struct node *start, int val)
         if (start->next != NULL)
         {
             temp->next = start->next;
+            end->next->next = temp;
         }
         else
         {
             temp->next = temp;
+            end->next = temp;
         }
         start->next = temp;
         temp->data = val;
@@ -25,31 +27,27 @@ int insertb(struct node *start, int val)
     }
     return 1;
 }
-int inserte(struct node *start, int val)
+int inserte(struct node *end, int val)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
-    struct node *end = start->next;
-    do
-    {
-        end = end->next;
-    } while (end->next != start->next);
     if (temp != NULL)
     {
-        end->next = temp;
         temp->data = val;
-        if (start->next != NULL)
+        if (end->next != NULL)
         {
-            temp->next = start->next;
+            temp->next = end->next->next;
+            end->next->next = temp;
         }
         else
         {
+            end->next = temp;
             temp->next = temp;
         }
         return 0;
     }
     return 1;
 }
-int insert(struct node *start, int val, int pos)
+int insert(struct node *start, struct node *end, int val, int pos)
 {
     if (pos < 0)
     {
@@ -59,13 +57,21 @@ int insert(struct node *start, int val, int pos)
     struct node *posNode = start;
     if (temp != NULL)
     {
+        if (posNode->next == NULL)
+        {
+            posNode->next = temp;
+            temp->next = temp;
+            temp->data = val;
+            end->next = temp;
+            return 0;
+        }
         for (int i = 0; i < pos; i++)
         {
             posNode = posNode->next;
-            if (posNode == NULL)
-            {
-                return 1;
-            }
+        }
+        if (posNode == end->next)
+        {
+            end->next = temp;
         }
         temp->next = posNode->next;
         temp->data = val;
@@ -74,18 +80,19 @@ int insert(struct node *start, int val, int pos)
     }
     return 1;
 }
-int deleteb(struct node *start)
+int deleteb(struct node *start, struct node *end)
 {
     struct node *temp = start->next;
     if (start->next != NULL)
     {
         start->next = start->next->next;
+        end->next->next = start->next;
         free(temp);
         return 0;
     }
     return 1;
 }
-int deletee(struct node *start)
+int deletee(struct node *start, struct node *end)
 {
     if (start->next == NULL)
     {
@@ -97,10 +104,11 @@ int deletee(struct node *start)
         temp = temp->next;
     }
     free(temp->next);
-    temp->next = NULL;
+    temp->next = start->next;
+    end->next = temp;
     return 0;
 }
-int delete(struct node *start, int pos)
+int delete(struct node *start, struct node *end, int pos)
 {
     if (pos < 0)
     {
@@ -158,7 +166,8 @@ int main()
 {
     int choice, val, pos;
     struct node *start = (struct node *)malloc(sizeof(struct node));
-    start->next = NULL;
+    struct node *end = NULL;
+    start->next = end;
     do
     {
         printf("1.INSERT BEGINNING\n2.INSERT END\n3.INSERT\n4.DELETE BEGINNING\n5.DELETE END\n6.DELETE\n7.IS EMPTY\n8.IS FULL\n9.DISPLAY\n10.SEARCH\n11.EXIT\nCHOOSE AN OPERATION: ");
@@ -168,30 +177,30 @@ int main()
         case 1:
             printf("ENTER ELEMENT : ");
             scanf("%d", &val);
-            insertb(start, val) ? printf("FAILED TO INSERT\n\n") : printf("INSERTED SUCCESSFULLY\n\n");
+            insertb(start, end, val) ? printf("FAILED TO INSERT\n\n") : printf("INSERTED SUCCESSFULLY\n\n");
             break;
         case 2:
             printf("ENTER ELEMENT : ");
             scanf("%d", &val);
-            inserte(start, val) ? printf("FAILED TO INSERT\n\n") : printf("INSERTED SUCCESSFULLY\n\n");
+            inserte(end, val) ? printf("FAILED TO INSERT\n\n") : printf("INSERTED SUCCESSFULLY\n\n");
             break;
         case 3:
             printf("ENTER ELEMENT : ");
             scanf("%d", &val);
             printf("ENTER INDEX : ");
             scanf("%d", &pos);
-            insert(start, val, pos) ? printf("FAILED TO INSERT\n\n") : printf("INSERTED SUCCESSFULLY\n\n");
+            insert(start, end, val, pos) ? printf("FAILED TO INSERT\n\n") : printf("INSERTED SUCCESSFULLY\n\n");
             break;
         case 4:
-            deleteb(start) ? printf("FAILED TO DELETE\n\n") : printf("DELETED SUCCESSFULLY\n\n");
+            deleteb(start, end) ? printf("FAILED TO DELETE\n\n") : printf("DELETED SUCCESSFULLY\n\n");
             break;
         case 5:
-            deletee(start) ? printf("FAILED TO DELETE\n\n") : printf("DELETED SUCCESSFULLY\n\n");
+            deletee(start, end) ? printf("FAILED TO DELETE\n\n") : printf("DELETED SUCCESSFULLY\n\n");
             break;
         case 6:
             printf("ENTER INDEX : ");
             scanf("%d", &pos);
-            delete (start, pos) ? printf("FAILED TO DELETE\n\n") : printf("DELETED SUCCESSFULLY\n\n");
+            delete (start, end, pos) ? printf("FAILED TO DELETE\n\n") : printf("DELETED SUCCESSFULLY\n\n");
             break;
         case 7:
             break;
