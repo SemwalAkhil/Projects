e7cef0fe90821ac098b46ee9ecb7c86a726e08e0
SemwalAkhil <akhil392semwal@gmail.com>
2024-11-05 16:00:26 +0530

    c site;5/11/24;1600

--- C SITE/index.js
+++ C SITE/index.js
@@ -1,7 +1,9 @@
 window.onscroll = function() {
   toggleTopButton();
 };
-
+window.onload = function(){
+  createFooter();
+}
 function toggleTopButton() {
   const topButton = document.getElementsByClassName("topBtn")[0];
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -15,3 +17,16 @@ function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+ function createFooter() {
+            // Create the footer container
+            const footer = document.createElement("footer");
+            footer.style.cssText = "color: #FEECB3; padding: 1px; text-align: center;";
+
+            // Add content to the footer
+            footer.innerHTML = `
+                <p>Akhil Semwal 4033/23</p>
+            `;
+
+            // Append the footer to the body
+            document.body.appendChild(footer);
+        }
