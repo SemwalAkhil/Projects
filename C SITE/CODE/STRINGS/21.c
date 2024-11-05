1f5f18b1fae2e36b8cef131d8e211500be239402
SemwalAkhil <akhil392semwal@gmail.com>
2024-11-05 11:38:22 +0530

    c site;5/11/24;1138

--- C SITE/CODE/STRINGS/21.c
+++ C SITE/CODE/STRINGS/21.c
@@ -1,31 +1,65 @@
-// TODO
 // 21. PRINT ALL THE PERMUTATION OF A STRING I.E. "ABCDE".
 #include <stdio.h>
-#define maxSize 100
-void printPerms(char str[maxSize], int size, int locked)
+
+int getStrLen(char str[])
 {
-    char perm[maxSize];
-    int count = 0;
-    if (locked < size)
+    int i;
+    for (i = 0; str[i] != 0; i++)
+        continue;
+    return i;
+}
+
+// Function to swap characters at positions i and j in the array
+void swap(char *str, int i, int j)
+{
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+}
+
+// Recursive function to generate permutations
+void generatePermutations(char *str, int start, int end)
+{
+    // Base case: if we've reached the end of the string
+    if (start == end)
     {
-        for (int i = locked; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
-                if (j != locked)
-                {
-                    str[j] = str[(j + i) % size];
-                }
-            }
-            printf("%s\n", str);
-        }
+        printf("%s\n", str);
+        return;
+    }
+
+    // Try all possible characters for current position
+    for (int i = start; i <= end; i++)
+    {
+        // Swap current character with character at position 'start'
+        swap(str, start, i);
+
+        // Recursively generate permutations for remaining characters
+        generatePermutations(str, start + 1, end);
+
+        // Backtrack: restore the string to its previous state
+        swap(str, start, i);
     }
 }
+
 int main()
 {
-    char str[maxSize];
-    printf("Enter string : ");
-    gets(str);
-    printPerms(str, 5, 0);
+    char str[100]; // Array to store input string
+
+    // Get string input from user
+    printf("Enter a string to generate all permutations: ");
+    scanf("%s", str);
+
+    int n = getStrLen(str); // Get length of input string
+
+    // Calculate total number of permutations
+    int total = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        total *= i;
+    }
+
+    printf("\nAll possible permutations (%d total):\n", total);
+    generatePermutations(str, 0, n - 1);
+
     return 0;
 }
--- C SITE/CODE/STRINGS/27.c
+++ C SITE/CODE/STRINGS/27.c
@@ -1,9 +1,74 @@
-// TODO
 // 27. DISPLAY THE STRING ON THE OUTPUT SCREEN BUT ASK THE TEXT ALIGNMENT OPTION GIVEN BY THE USER (1 FOR LEFT, 2 FOR CENTER, 3 RIGHT)
 #include <stdio.h>
 
+int getStrLen(char str[])
+{
+    int i;
+    for (i = 0; str[i] != 0; i++)
+        continue;
+    return i;
+}
+// Function to print spaces
+void printSpaces(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        printf(" ");
+    }
+}
+
+// Function to print text with specified alignment
+void printAlignedText(char *text, int width, int alignment)
+{
+    int len = getStrLen(text);
+    int spaces;
+
+    switch (alignment)
+    {
+    case 1: // Left alignment
+        printf("%s\n", text);
+        break;
+
+    case 2: // Center alignment
+        spaces = (width - len) / 2;
+        printSpaces(spaces);
+        printf("%s\n", text);
+        break;
+
+    case 3: // Right alignment
+        spaces = width - len;
+        printSpaces(spaces);
+        printf("%s\n", text);
+        break;
+
+    default:
+        printf("Invalid alignment option!\n");
+    }
+}
+
 int main()
 {
+    char text[1000];
+    int choice;
+    const int SCREEN_WIDTH = 80; // Assuming standard terminal width
+
+    // Get the text from user
+    printf("Enter the text: ");
+    gets(text); // Using gets() for demonstration. In production code, fgets() is safer
+
+    // Display menu
+    printf("\nChoose text alignment:\n");
+    printf("1. Left Alignment\n");
+    printf("2. Center Alignment\n");
+    printf("3. Right Alignment\n");
+    printf("Enter your choice (1-3): ");
+    scanf("%d", &choice);
+
+    printf("\nFormatted output:\n");
+    printf("----------------\n");
+
+    // Print the text with chosen alignment
+    printAlignedText(text, SCREEN_WIDTH, choice);
 
     return 0;
 }
