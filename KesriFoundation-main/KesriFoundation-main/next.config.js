6861736e0d61b019541f5dd941bbc959767fe080
SemwalAkhil <akhil392semwal@gmail.com>
2025-02-06 21:01:33 +0530

    updating repo

--- KesriFoundation-main/KesriFoundation-main/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-// - name: Static HTML export with Next.js
-// run: ${{ steps.detect-package-manager.outputs.runner }} next export
--- KesriFoundation-main/KesriFoundation-main/src/prefix.ts
+++
@@ -1,3 +0,0 @@
-const prefix ='VishakhaFoundation';
-
-export { prefix };
