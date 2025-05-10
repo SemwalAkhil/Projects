d04d440408c6db24257172a43e158f86f03c51a6
SemwalAkhil <akhil392semwal@gmail.com>
2025-05-10 14:58:35 +0530

    Clearing Kotlin

--- Kotlin/CharBool/src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val myChar = 'a'
-    val maxCharValue = Char.MAX_VALUE
-    val minCharValue = Char.MIN_VALUE
-    println("MyChar : $myChar \nMax char value : $maxCharValue\nMin char value : $minCharValue")
-    val myBool = true
-    val myBool2 = false
-    println("MyBool : $myBool MyBool2 : false")
-}
--- Kotlin/Variables.kt
+++
@@ -1,4 +0,0 @@
-fun main(){
-    var userName: String = "Ak"
-    println("Hello $userName!")
-}
